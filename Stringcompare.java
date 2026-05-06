fa2a9029f5e17431b1ebdf4c0c4cf482a32d7143
LaxmiPrasanna3109 <24B81A6620@cvr.ac.in>
2026-05-06 21:28:01 +0530

    Create Stringcompare.java

---
+++ Stringcompare.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testString() {
+        String str = "check the string";
+        assertEquals("check the string", str);
+    }
+}
